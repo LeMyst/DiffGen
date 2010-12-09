52ba1ed3b44e65f6c1984884ed0ade5868316980
Shinryo <Shinryo@10941dc7-832e-4034-924b-dc8736a8d1d6>
2010-12-09 21:00:27 +0000

    - Fixed SkipServiceSelect * Function SelectKoreaClientInfo() is no longer used to initiate connection, instead it calls SelectClientInfo() like for every other service type.

--- DiffGen/Patches/SkipServiceSelect.php
+++ DiffGen/Patches/SkipServiceSelect.php
@@ -15,6 +15,20 @@ function SkipServiceSelect($exe){
 	// Skip short jump
 	$exe->replace($offset, array(0 =>	"\x90\x90"));
 	
+	// Shinryo:
+	// Gravity has their clientinfo hardcoded and seperated the initialization, screw 'em.. :(
+	// Therefore just replace (jump over) their call with a standard one which parses clientinfo.xml
+	// SelectKoreaClientInfo() -> SelectClientInfo()
+	// TO-DO: Maybe use a seperate diff? Dunno.
+	$code	=	"\xE8\xAB\xAB\xFF\xFF\xE9\xAB\xAB\xFF\xFF\x6A\x00\xE8\xAB\xAB\xFF\xFF\x83\xC4\x04";
+	$offset	=	$exe->code($code,	"\xAB");
+	if ($offset	===	false) {
+		echo "Failed in	part 2";
+		return false;
+	}
+	
+	$exe->replace($offset, array(0 =>	"\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90"));
+	
 	return true;
 }
 ?>
