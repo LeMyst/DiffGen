3d1383e38004dbfedc456cd4cff8c1848e4c92b2
Shinryo <Shinryo@10941dc7-832e-4034-924b-dc8736a8d1d6>
2010-12-10 23:42:32 +0000

    - Fixed IncreaseViewID

--- DiffGen/Patches/IncreaseViewID.php
+++ DiffGen/Patches/IncreaseViewID.php
@@ -1,24 +1,27 @@
 <?php
+// 10.12.2010 - As far as I see those three codes that were searched before aren't necessary in VC9 [Shinryo]
+
     function IncreaseViewID($exe) {
         if ($exe === true) {
             return "[Add]_Increase_Headgear_ViewID_to_2000";
         }
-        $codes = array(
-        "\xFF\xB9\xE8\x03\x00\x00\x2B\xC8\x51",
-        "\xFF\x3D\xE8\x03\x00\x00\x76\x15\x8B",
-        "\x81\xFE\xE8\x03\x00\x00\x7C\xB8\x8B",
-        );
-        $codeoffsets = array(2,2,2);
-        $changes = array("\xD0\x07","\xD0\x07","\xD0\x07");
-        foreach ($codes as $index => $code) {
-            $offset = $exe->code($code, "\xAB");
-            if ($offset === false) {
-                echo "Failed in part $index";
-                return false;
-            } else {
-                $exe->replace($offset, array($codeoffsets[$index] => $changes[$index]));
-            }
-        }
+        
+        // In case of break:
+        // Search for "ReqAccName" and search somewhere below for the
+        // maximum ViewID.
+        // inc     eax
+				// cmp     eax, <maxViewID>
+				// mov     [esp+4Ch+Src], eax
+				// jl      short <location>                
+				$code	=	"\x40\x3D\xE8\x03\x00\x00\x89\x44\x24\xAB\x7C\x9F";
+				$offset	=	$exe->code($code,	"\xAB");
+				if ($offset	===	false) {
+					echo "Failed in	part 1";
+					return false;
+				}
+
+				$exe->replace($offset, array(2 =>	"\xD0\x07"));
+				
         return true;
     }
 ?>
