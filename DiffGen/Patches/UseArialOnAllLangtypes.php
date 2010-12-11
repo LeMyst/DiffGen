b022af22070a4092cca8abef1d5f7e37eb1aece0
Shinryo <Shinryo@10941dc7-832e-4034-924b-dc8736a8d1d6>
2010-12-11 21:00:55 +0000

    - UnlimitedChatFlood is now AllowChatFloodUnlimited as it should be the same group as the other ChatFlood Diffs - Modified UseArialOnAllLangtypes to always use Arial (how it was supposed to be)

--- DiffGen/Patches/UseArialOnAllLangtypes.php
+++ DiffGen/Patches/UseArialOnAllLangtypes.php
@@ -1,19 +1,30 @@
 <?php
+// 10.12.2010 - Changed behaviour of this diff to always (in any case) use Arial on all language types. [Shinryo]
+
     function UseArialOnAllLangtypes($exe) {
         if ($exe === true) {
             return "[UI](9)_Use_Arial_on_All_Langtypes";
         }
-        $code =  "\x83\xFA\x0A"                 // cmp     edx, 0Ah
-                ."\x0F\x87\xAB\x00\x00\x00"     // ja      loc_40899B
-                ."\xFF\x24\x95\xAB\xAB\xAB\xAB" // jmp     ds:off_4089E0[edx*4] ; switch jump
-                ."\xA1\xAB\xAB\xAB\xAB"         // mov     eax, Langtype
-                ."\x83\xF8\x06";                // cmp     eax, 6
+        $code =  "\x75\x22"       // JNE SHORT <current+22>
+                ."\x83\xF8\x14"   // CMP EAX,14
+                ."\x7C\x1D"       // JL SHORT <current+1D>
+                ."\x89\x41\x0C";  // MOV DWORD PTR DS:[ECX+0C],EAX
+                
+        // Overwrite both conditions and set variables so that it will use Arial
+        $replace = "\x31\xD2"     // XOR EDX,EDX
+                  ."\x83\xC2\x0F" // ADD EDX,0F
+                  ."\x31\xC0"     // XOR EAX,EAX
+                  ."\x40"         // INC EAX
+                  ."\xEB\x1E";    // JMP SHORT <current+1E>
+                  
         $offset = $exe->code($code, "\xAB");
         if ($offset === false) {
             echo "Failed in part 1";
             return false;
         }
-        $exe->replace($offset, array(3 => "\xEB\x10"));
+        
+        $exe->replace($offset, array(0 => $replace));
+
         return true;
     }
 ?>
