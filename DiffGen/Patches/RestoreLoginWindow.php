a3c90be92fb9d0d23572815c042d1c2075128b74
Shinryo <Shinryo@10941dc7-832e-4034-924b-dc8736a8d1d6>
2010-12-13 11:02:36 +0000

    - Modified RestoreLoginWindow Part 3 to use a generic approach * It works with all VC9 clients so far

--- DiffGen/Patches/RestoreLoginWindow.php
+++ DiffGen/Patches/RestoreLoginWindow.php
@@ -51,7 +51,7 @@
                 ."\x90";
         $exe->replace($offset, array(24 => $code));
         
-        // Force the client to send old login packet
+        // Force the client to send old login packet.
         $code =  "\x80\x3D\xAB\xAB\xAB\x00\x00" // cmp     g_passwordencrypt, 0
                 ."\x0F\xAB\xAB\xAB\x00\x00"     // jnz     loc_62072D
                 ."\xA1\xAB\xAB\xAB\x00"         // mov     eax, Langtype
@@ -78,15 +78,30 @@
         // This shall fix this behaviour by aborting the quit operation,
         // set the return mode to 3 (login) and pass 10013 as idle value.
         // It was handy that "this" pointer was passed before. :)
-        $code =  "\xB9\xAB\xAB\xAB\x00"                           // MOV ECX,<address>
-                ."\xE8\xAB\xAB\xAB\xFF"                           // CALL Quit()
-                ."\xC7\x84\x24\xCC\x00\x00\x00\xFF\xFF\xFF\xFF"   // MOV DWORD PTR SS:[ESP+0CC],-1
-                ."\x8D\x8C\x24\xAB\x00\x00\x00"                   // LEA ECX,[ESP+0A4]
-                ."\xFF\x15\xAB\xAB\xAB\x00"                       // CALL DWORD PTR DS:[<address>]
-                ."\x8B\x8C\x24\xAB\xAB\x00\x00"                   // MOV ECX,DWORD PTR SS:[ESP+0C4]
-                ."\x64\x89\x0D\x00\x00\x00\x00";                  // MOV DWORD PTR FS:[0],ECX
+        $code =  "\x8B\xF1"                     // MOV ESI,ECX
+                ."\x8B\x46\x04"                 // MOV EAX,DWORD PTR DS:[ESI+4]
+                ."\xC7\x40\x14\x00\x00\x00\x00" // MOV DWORD PTR DS:[EAX+14],0
+                ."\x83\x3D\xAB\xAB\xAB\x00\x0B" // CMP DWORD PTR DS:[<address>],0B
+                ."\x75\xAB"                     // JNE SHORT 0054A7D3
+                ."\x8B\x0D\xAB\xAB\xAB\x00"     // MOV ECX,DWORD PTR DS:[<address>]
+                ."\x6A\x01"                     // PUSH 1
+                ."\x6A\x00"                     // PUSH 0
+                ."\x6A\x00"                     // PUSH 0
+                ."\x68\xAB\xAB\xAB\x00"         // PUSH <offset>  ; ASCII "http://www.ragnarok.co.in/index.php"
+                ."\x68\xAB\xAB\xAB\x00"         // PUSH <offset>  ; ASCII "open"
+                ."\x51"                         // PUSH ECX
+                ."\xFF\x15\xAB\xAB\xAB\x00"     // CALL DWORD PTR DS:[<address>]  ; ShellExecuteA
+                
+                // Shinryo:
+                // The easierst way would be propably to set this value to a random value instead of 0,
+                // but the client would dimmer down/flicker and appear again at login interface.
+                // I prefer the old way that the client used.
+                ."\xC7\x06\x00\x00\x00\x00";      // MOV DWORD PTR DS:[ESI],0 <----- Return to which mode
         
-        $replace =   "\x8B\x4C\xE4\x08"             // MOV ECX,DWORD PTR SS:[ESP+8]
+        $replace =  "\x8B\x4C\xE4\x10"              // MOV ECX,DWORD PTR SS:[ESP+10]
+                    // Save the used registers this time..
+                    ."\x52"                         // PUSH EDX
+                    ."\x50"                         // PUSH EAX
                     ."\x8B\x11"                     // MOV EDX,DWORD PTR DS:[ECX]
                     ."\x8B\x42\x18"                 // MOV EAX,DWORD PTR DS:[EDX+18]
                     ."\x6A\x00"                     // PUSH 0
@@ -95,22 +110,15 @@
                     ."\x68\x1D\x27\x00\x00"         // PUSH 271D
                     ."\xC7\x41\x0C\x03\x00\x00\x00" // MOV DWORD PTR DS:[ECX+0C],3
                     ."\xFF\xD0"                     // CALL EAX
-                    ."\x90\x90\x90\x90\x90\x90\x90" // NOPS
-                    ."\x90\x90\x90\x90\x90\x90\x90" // NOPS
-                    ."\x90\x90\x90\x90\x90";        // NOPS                    
+                    // ..and restore them again.
+                    ."\x58"                         // POP EAX
+                    ."\x5A"                         // POP EDX
+                    .str_repeat("\x90", 23);        // NOPS                        
                     
         $offset = $exe->match($code, "\xAB");
         if ($offset === false) {
-            // Shinryo: Silently fail this one,
-            // since those old diffs don't have priority at the moment
-            // and don't really need this additional feature.
-            //
-            // echo "---> Failed in part 4 - continue anyway..";
-            //
-            // Shinryo:
-            // For the time beeing, just allow this diff anyway.
-            // Have to make some tests first.
-            return true;
+            echo "Failed in part 4";
+            return false;
         }
 
         $exe->replace($offset, array(0 => $replace));
