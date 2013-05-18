2a1af1badcf61868162afa52d5396862eecdbd8d
Shakto <Shakto@10941dc7-832e-4034-924b-dc8736a8d1d6>
2013-05-18 11:07:05 +0000

    Fix of custom aura sprites use

--- xDiffGen2/Patches/UseCustomAuraSprites.php
+++ xDiffGen2/Patches/UseCustomAuraSprites.php
@@ -10,21 +10,45 @@ Enable this to used aurafloat.tga, auraring.bmp and freezing_circle.bmp as aura 
 The default aura files are pikapika2.bmp, blue_ring.tga and freezing_circle.bmp.");
         }
         $free = 0x380;
-        $code =  "\x68" . pack("I", $exe->str("effect\\ring_blue.tga","rva"))
-                ."\x8B\xCE"                 // mov     ecx, esi        ; int
-                ."\xE8\xAB\xAB\xAB\xAB"
-                ."\xE9\xAB\xAB\xAB\xAB"
-                ."\x6A\x00"
-                ."\x68" . pack("I", $exe->str("effect\\pikapika2.bmp","rva"));
-        $offset = $exe->code($code, "\xAB");
-        if ($offset === false) {
+		
+        $code00 =  "\x68" . pack("I", $exe->str("effect\\ring_blue.tga","rva"))	// PUSH    "effect\ring_blue.tga"
+                ."\xFF\x15\xAB\xAB\xAB\xAB"                 					// CALL    NEAR DWORD PTR DS:[&MSVCP90.std::basic_string<char>::basic_string<char>]
+                ."\x89\xAB\xAB\xAB"												// MOV     [esp+44h], ebp
+                ."\xC7\x44\xAB\xAB\xAB\xAB\xAB\xAB"								// MOV     dword ptr [esp+44h], 0FFFFFFFFh
+                ."\x8B\xCE"														// MOV     ECX,ESI
+				."\xE8\xAB\xAB\xAB\xAB"											// CALL    ADDR
+				."\x8B\x57\xAB"													// MOV     EAX,DWORD PTR DS:[EDI+CONST]
+				."\x56"															// PUSH    esi
+				."\x8B\xCF"														// MOV     ecx, edi
+				."\x89\xAB\xAB"													// mov     [esi+4], edx
+				."\x89\xAB\xAB"													// mov     [esi+0Ch], ebx
+				."\x89\xAB\xAB"													// mov     [esi+10h], ebp
+				."\xC7\x46\xAB\xAB\xAB\xAB\xAB"									// mov     dword ptr [esi+8], 1
+				."\xE8\xAB\xAB\xAB\xAB";										// call    sub_62A440
+			
+        $code01 = "\x68" . pack("I", $exe->str("effect\\pikapika2.bmp","rva"))	// PUSH    "effect\pikapika2.bmp"
+				 ."\xFF\x15";													// CALL    NEAR DWORD PTR DS:[&MSVCP90.std::basic_string<char>::basic_string<char>]
+				 
+        $offset00 = $exe->code($code00, "\xAB");
+        if ($offset00 === false) {
+            echo "Failed in part 1";
+            return false;
+        }
+		
+        $offset01 = $exe->code($code01, "\xAB");
+        if ($offset01 === false) {
             echo "Failed in part 2";
             return false;
         }
-        $exe->replace($offset, array(1 => pack("I", ($exe->imagebase() + $free)), 20 => pack("I", ($exe->imagebase() + $free + 21))));
+		//$offset01b = $offset01 + 1 - $offset00;
+		
+        $exe->replace($offset00, array(1 => pack("I", ($exe->imagebase() + $free))));
+		$exe->replace($offset01, array(1 => pack("I", ($exe->imagebase() + $free + 21))));
+		
         $code =  "effect\aurafloat.tga\x00"
                 ."effect\auraring.bmp\x00\x90";
         $exe->insert($code, $free);
+		
         return true;
     }
 ?>
