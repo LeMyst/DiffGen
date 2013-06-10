360bbc230521bbdadc639cd8de9c81a5f54ae50e
Shakto <Shakto@10941dc7-832e-4034-924b-dc8736a8d1d6>
2013-06-10 10:01:56 +0000

    Small fix and removing chinese font who isn't working

--- xDiffGen2/Patches/QuakeSkillEffect.php
+++ xDiffGen2/Patches/QuakeSkillEffect.php
@@ -3,8 +3,17 @@ function QuakeSkillEffect($exe) {
     if ($exe === true) {
         return new xPatch(67, 'Disable Quake skill effect', 'UI', 0, '');
     }
-
-    $code =  "\xD9\x44\x24\x04\xD9\x59\x04\xD9\x44\x24\x0C\xD9\x59\x0C\xD9\x44\x24\x08\xD9\x59\x08\xC2\x0C\x00\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\x8B\x44\x24";
+	
+	// MISSION: Find CView::SetQuake and CView::SetQuakeInfo.
+	// You are pretty much lost, if you are not able to hunt
+	// either of them down, as they are next to each other. One
+	// VC6 hint being: Look for PUSH 3E4CCCCDh, PUSH 3E75C28F
+	// and PUSH 3F800000h. The next call after these 3 PUSHs is
+	// CView::SetQuake, right above it is CView::SetQuakeInfo.
+	// VC9 does not push float values like longs, but pull them
+	// out of somewhere. The tail of CView::SetQuake can serve
+	// for comparison.
+    $code =  "\xD9\x44\x24\x04\xD9\x59\x04\xD9\x44\x24\x0C\xD9\x59\x0C\xD9\x44\x24\x08\xD9\x59\x08\xC2\x0C\x00\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\x8B\x44\x24\x04";
           
     $offset = $exe->match($code, "\xAB");
 
@@ -13,7 +22,8 @@ function QuakeSkillEffect($exe) {
         return false;
     }
 
-    $exe->replace($offset, array(0 => "C2\x0C\x00\x04\xD9\x59\x04\xD9\x44\x24\x0C\xD9\x59\x0C\xD9\x44\x24\x08\xD9\x59\x08\xC2\x0C\x00\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xC2\x14\x00"));
+    $exe->replace($offset, array(0 => "\xC2\x0C\x00"));
+	$exe->replace($offset, array(32 => "\xC2\x14\x00"));
     return true;
 	
 }
