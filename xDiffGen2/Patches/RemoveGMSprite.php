57cd52a42ea45903ccf16c273ef91d2c79ced5ef
NeoMind <NeoMind@10941dc7-832e-4034-924b-dc8736a8d1d6>
2013-07-08 06:37:02 +0000

    Rewrote RemoveGMSprite for using better method for finding the patterns, working irrespective of clientdate.

--- xDiffGen2/Patches/RemoveGMSprite.php
+++ xDiffGen2/Patches/RemoveGMSprite.php
@@ -5,38 +5,38 @@
 			return new xPatch(96, 'Remove GM Sprites', 'UI', 0,
 			'Remove the GM sprites and keeping all the functionality like yellow name and admin right click.');
 
-		// 2 matches	                            
-		if ($exe->clientdate() <= 20130605){
-			$code =  "\x83\xC4\x04"					// add     esp, 4
-					."\x84\xC0"						// test    al, al
-					."\x0F\x84\xE3\x00\x00\x00";	// jz      loc_80F81E
-		}
-		else {
-			$code =  "\x83\xC4\x04"					// add     esp, 4
-					."\x84\xC0"						// test    al, al
-					."\x0F\x84\x5B\x01\x00\x00";	// jz      loc_80F81E	
-		}
-
-		$offsets = $exe->matches($code, "\xAB", 0);
+		$offsets = array();
+		//Step 1 : Find RVA of GM spr file
+		$code = "\xC0\xCE\xB0\xA3\xC1\xB7\x5C\xBF\xEE\xBF\xB5\xC0\xDA\x5C\xBF\xEE\xBF\xB5\xC0\xDA\x32\x5F\xB3\xB2\x5F\xB0\xCB" . ".Spr";
+				//ÀÎ°£Á·\¿î¿µÀÚ\¿î¿µÀÚ2_³²_°Ë.Spr
+				
+		$offsets[] = pack("I", $exe->str($code,"rva"));
+		
+		//Step 2 : Find RVA of GM act file
+		$code = "\xC0\xCE\xB0\xA3\xC1\xB7\x5C\xBF\xEE\xBF\xB5\xC0\xDA\x5C\xBF\xEE\xBF\xB5\xC0\xDA\x32\x5F\xB3\xB2\x5F\xB0\xCB" . ".Act";
+				//ÀÎ°£Á·\¿î¿µÀÚ\¿î¿µÀÚ2_³²_°Ë.Act
+		
+		$offsets[] = pack("I", $exe->str($code,"rva"));
 	
-        if (count($offsets) != 2) {
-            echo "Failed in part 1";
-            return false;
-        }
-		
-		if ($exe->clientdate() <= 20130605){
-			$exe->replace($offsets[0], array(5 => "\xE9\xE4\x00\x00\x00\x90")); // jmp     loc_80F81E & nop
-			$exe->replace($offsets[1], array(5 => "\xE9\xE4\x00\x00\x00\x90")); //
-		}
-		else {
-			$exe->replace($offsets[0], array(5 => "\xE9\x5C\x01\x00\x00\x90")); // jmp     loc_80F81E & nop (15C)
-			$exe->replace($offsets[1], array(5 => "\xE9\x5C\x01\x00\x00\x90")); //		
-		}
-		
-		//It have to jump here : just count the offset number between both -5
-		//8B 4C 24 14                                   mov     ecx, [esp+arg_4]
-		// 51                                            push    ecx
-
-		
+		foreach($offsets as $offset)
+		{
+			//Step 3 : Find Pushed location
+			$code = "\x68" . $offset;
+			$finish = $exe->code($code, "");
+			
+			//Step 4 : Find Pattern within boundary from finish (lets say within 0x200 bytes)
+			$code = "\x83\xC4\x04"				//add esp, 4
+					."\x84\xC0"					//test al,al
+					."\x0F\x84\xAB\xAB\x00\x00";	//jz <location skipping GM sprite override>
+					
+			$location = $exe->match($code, "\xAB", $finish - 0x200, $finish);
+			
+			//Step 5 : Prep pattern for replace
+			$diff = $exe->read($location + 7, 4, "I");
+			$code = "\xE9" . pack("I", $diff + 1) . "\x90";
+			
+			//Step 6 : Replace pattern
+			$exe->replace($location, array(5 => $code));
+		}		
 	}
 ?>
